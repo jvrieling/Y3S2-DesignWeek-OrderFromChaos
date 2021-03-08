5231ef1d9fc270b0a5a0d02b3b1d5ef34edb3d7e
jvrieling <justin.vrieling@hotmail.com>
2021-03-08 16:15:16 -0500

    Import a camera script and set stuff up

---
+++ Y3S2-OrderFromChaos-DesignWeek/Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,31 @@
+﻿//Got this script from https://forum.unity.com/threads/click-drag-camera-movement.39513/
+//Author: damien_oconnell
+
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+
+        
+
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+        transform.Translate(move, Space.World);
+    }
+
+
+}
