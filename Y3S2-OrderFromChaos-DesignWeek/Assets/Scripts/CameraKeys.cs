8b1639f49f64d339411333cd4c6505508915adab
Samuel Barnes <barnsamu@sheridancollege.ca>
2021-03-09 15:46:04 -0500

    Create CameraKeys script
    This script allows you to move the camera left and right with the keyboard. In the inspector, you can change how fast the camera moves as well as how far left and right it can move.

---
+++ Y3S2-OrderFromChaos-DesignWeek/Assets/Scripts/CameraKeys.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraKeys : MonoBehaviour
+{
+    public float cameraSpeed = 10; // Determines how fast the camera moves while pressing keys
+    public float cameraMinimumX = 0; // Determines how far left the camera can move
+    public float cameraMaximumX = 50; // Determines how far right the camera can move
+
+    void Update()
+    {
+        // Get input
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // Move camera
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * cameraSpeed * Time.deltaTime);
+
+
+        // If camera is farther left than is allowed...
+        if (transform.position.x < cameraMinimumX)
+        {
+            // Set x position to minimum
+            transform.position = new Vector3(cameraMinimumX, transform.position.y, transform.position.z);
+        }
+
+
+        // If camera is farther right than is allowed...
+        if (transform.position.x > cameraMaximumX)
+        {
+            // Set x position to maximum
+            transform.position = new Vector3(cameraMaximumX, transform.position.y, transform.position.z);
+        }
+    }
+}
