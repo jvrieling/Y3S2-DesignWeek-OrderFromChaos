03b84194ea4bc63420c3c51e6ada951350d16031
Owen Darling <darlinow@sheridancollege.ca>
2021-03-09 16:09:30 -0500

    drag and drop temp

---
+++ Y3S2-OrderFromChaos-DesignWeek/Assets/Scripts/Slot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    //private DragDrop dragDrop;
+   //public int num;
+   //public int matchNum;
+   /*public void Match(int num)
+    {
+        OnDrop();
+    }
+   */
+   public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("dropping");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
